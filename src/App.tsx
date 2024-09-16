9f41884c7d7d85e5654e3f32c6bab585fc18be41
Jeff <mason8971@gmail.com>
2024-09-16 11:49:57 -0400

    2.xx_practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Labs from './Labs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Jeff's World! 
-          This is a test of the React App running in a Docker container.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Labs />
     </div>
   );
 }
