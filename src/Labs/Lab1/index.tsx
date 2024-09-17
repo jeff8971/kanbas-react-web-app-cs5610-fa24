a05672a818dea51b4742ab537e01637707847bfe
Jeff <mason8971@gmail.com>
2024-09-17 00:27:16 -0400

    unsorted list

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -47,16 +47,28 @@ export default function Lab1() {
       <div id="wd-lists">
         <h4>List Tags</h4>
         <h5>Ordered List Tag</h5>
-        How to make pancakes:
-        1. Mix dry ingredients.
-        2. Add wet ingredients.
-        3. Stir to combine.
-        4. Heat a skillet or griddle.
-        5. Pour batter onto the skillet.
-        6. Cook until bubbly on top.
-        7. Flip and cook the other side.
-        8. Serve and enjoy!
-        </div>
+        <ol id="wd-pancakes">
+          <li>Mix dry ingredients.</li>
+          <li>Add wet ingredients.</li>
+          <li>Stir to combine.</li>
+          <li>Heat a skillet or griddle.</li>
+          <li>Pour batter onto the skillet.</li>
+          <li>Cook until bubbly on top.</li>
+          <li>Flip and cook the other side.</li>
+          <li>Serve and enjoy!</li>
+        </ol>
+      </div>
+      <div id="wd-tables">
+        <h5>Unordered List Tag</h5>
+        My favorite books (in no particular order)
+        <ul id="wd-my-books">
+          <li>Dune</li>
+          <li>Lord of the Rings</li>
+          <li>Ender's Game</li>
+          <li>Red Mars</li>
+          <li>The Forever War</li>
+        </ul>
+      </div>
   </div>
   );
-  }
+}
