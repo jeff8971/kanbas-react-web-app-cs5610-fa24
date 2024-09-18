89baa22d255ce008c1650935f6cd357b037326f3
Jeff <mason8971@gmail.com>
2024-09-18 01:09:30 -0400

    start at 3.31

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -1,14 +1,12 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 export default function Signin() {
-  return (
-    <div id="signin-screen">
-      <h3>Sign in</h3>
-      <input id="wd-username" placeholder="Username" /> <br />
-      <input id="wd-password" placeholder="Password" type="password"/> <br /> 
-      <Link id="wd-signin-btn"
-        to="/Kanbas/Account/Profile">Sign in</Link> <br />
-      <Link id="wd-signup-link"
-        to="/Kanbas/Account/Signup">Sign up</Link>
-    </div>
-  );
-}
+return (
+  <div id="wd-signin-screen">
+    <h3>Sign in</h3>
+    <input id="wd-username" placeholder="username" /> <br />
+    <input id="wd-password" placeholder="password" type="password" /> <br />
+    <Link id="wd-signin-btn"
+    to="/Kanbas/Account/Profile"> Sign in </Link> <br />
+    <Link id="wd-signup-link" to="/Kanbas/Account/Signup">Sign up</Link>
+  </div>
+);}
--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,9 +1,27 @@
+import { Routes, Route, Navigate } from "react-router";
+import AccountNavigation from "./Navigation";
 import Signin from "./Signin";
-export default function Account() { 
+import Profile from "./Profile";
+import Signup from "./Signup";
+
+export default function Account() {
   return (
-    <div id="wd-account-screen">
-      <h1>Account</h1>
-      <Signin />
-    </div>
-  );
-}
+  <div id="wd-account-screen">
+    <table>
+    <tr>
+      <td valign="top">
+        <AccountNavigation />
+      </td>
+      <td valign="top">
+      <Routes>
+        <Route path="/"
+              element={<Navigate to="/Kanbas/Account/Signin" />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Signup" element={<Signup />} />
+      </Routes>
+      </td>
+    </tr>
+    </table>
+  </div>
+  );}
