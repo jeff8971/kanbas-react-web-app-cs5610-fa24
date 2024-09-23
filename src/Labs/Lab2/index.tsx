e1c107fdc07b8956a72f2da984fbfa1defba3c4a
Jeff Zhao <zhao.yuan2@northeastern.edu>
2024-09-22 23:49:40 -0400

    finish 3

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -16,6 +16,8 @@ import BootstrapGrids from "./BootstrapGrids";
 import ScreenSizeLabel from "./ScreenSizeLabel";
 import BootstrapTables from "./BootstrapTables";
 import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+import BootstrapNavigation from "./BootstrapNavigation";
 
 export default function Lab2() {
   return (
@@ -76,6 +78,8 @@ export default function Lab2() {
         <ScreenSizeLabel />
         <BootstrapTables />
         <BootstrapLists />
+        <BootstrapForms />
+        <BootstrapNavigation />
         
       </div>
       </div>
