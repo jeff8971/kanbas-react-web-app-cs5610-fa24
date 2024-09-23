a573b616ce1b21a3f9e22c316024085efc3234e4
Jeff Zhao <zhao.yuan2@northeastern.edu>
2024-09-22 22:51:39 -0400

    3.3.3 Laying out content with grids a2

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -12,9 +12,10 @@ import Float from "./Float";
 import GridLayout from "./GridLayout";
 import Flex from "./Flex";
 import ReactIconsSampler from "./ReactIcons";
+import BootstrapGrids from "./BootstrapGrids";
 export default function Lab2() {
   return (
-  <div id="wd-lab2">
+  <div className="container">
     <h2>Lab 2 - Cascading Style Sheets</h2>
     <h3>Styling with the STYLE attribute</h3>
     <p>
@@ -67,6 +68,7 @@ export default function Lab2() {
         <GridLayout />
         <Flex />
         <ReactIconsSampler />
+        <BootstrapGrids/>
         
       </div>
       </div>
