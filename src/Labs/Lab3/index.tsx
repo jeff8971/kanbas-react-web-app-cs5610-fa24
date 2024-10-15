f860a86eca973ac7040ba07e67540521a4e7f837
jeff8971 <mason8971@gmaill.com>
2024-10-15 00:14:42 -0400

    2.3 JavaScript Functions -a3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,6 +1,11 @@
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
-
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import LegacyFunctions from "./LegacyFunctions";
 
 
 
@@ -10,6 +15,12 @@ export default function Lab3() {
       <h2>Lab 3</h2>
       <VariablesAndConstants/>
       <VariableTypes/>
+      <BooleanVariables/>
+      <IfElse/>
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <LegacyFunctions />
     </div>
   );
 }
