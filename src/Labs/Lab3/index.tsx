10e11f70846ea351b7fe88ffbfc5c22db5751887
jeff8971 <mason8971@gmaill.com>
2024-10-15 00:00:24 -0400

    2.2.1 Variables and Constants - a3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,10 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+
 export default function Lab3() {
   return (
-    <div id="wd-lab3">
-      <h3>Lab 3</h3>
+    <div>
+      <h2>Lab 3</h2>
+      <VariablesAndConstants/>
     </div>
   );
 }
