4f14cbf72b5e491e4e44faead127f6eefde07dc2
Jeff Zhao <mason8971@gmail.com>
2024-10-30 15:31:37 -0400

    dashboard 1st done

--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import modulesReducer from "./Courses/Modules/reducer";
+//import assignmentsReducer from "./Courses/Assignments/reducer";
 import accountReducer from "./Account/reducer";
+import enrollmentsReducer from "./Dashboard/reducer";
+import modulesReducer from "./Courses/Modules/reducer";
 
 const store = configureStore({
-  reducer: {
-    modulesReducer,
-    accountReducer,
-  },
+    reducer: {
+        //assignmentsReducer,
+        accountReducer,
+        enrollmentsReducer,
+        modulesReducer,
+    },
 });
 
 export default store;
