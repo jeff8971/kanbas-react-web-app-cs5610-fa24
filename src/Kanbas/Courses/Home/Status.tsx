f4577bb4bc0b07c1c6360ee647c2cf195ee19247
jeff8971 <zhao.yuan2@northeastern.edu>
2024-10-31 00:55:01 -0400

    3.5  modify the modules stuff -a4

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -7,25 +7,33 @@ import { RiSurveyLine } from "react-icons/ri";
 import { MdNotifications } from "react-icons/md";
 import { FaBullhorn } from "react-icons/fa";
 import { AiOutlineLineChart } from "react-icons/ai";
+import { useSelector } from "react-redux";
 
 export default function CourseStatus() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+
   return (
     <div id="wd-course-status" style={{ width: "300px" }}>
-      <h2>Course Status</h2>
-      <div className="d-flex">
-        <div className="w-50 pe-1">
-          <button className="btn btn-lg btn-secondary w-100 text-nowrap">
-            <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish
-          </button>
-        </div>
-        <div className="w-50">
-          <button className="btn btn-lg btn-success w-100">
-            <FaCheckCircle className="me-2 fs-5" /> Publish
-          </button>
-        </div>
-      </div>
-      <br />
+      {currentUser.role === 'FACULTY' && (
+        <>
+          <h2>Course Status</h2>
+          <div className="d-flex">
+            <div className="w-50 pe-1">
+              <button className="btn btn-lg btn-secondary w-100 text-nowrap">
+                <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish
+              </button>
+            </div>
+            <div className="w-50">
+              <button className="btn btn-lg btn-success w-100">
+                <FaCheckCircle className="me-2 fs-5" /> Publish
+              </button>
+            </div>
+          </div>
+          <br />
+        </>
+      )}
 
+      {/* Buttons visible to all users */}
       <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
         <BiImport className="me-2 fs-5" /> Import Existing Content
       </button>
--- src/Kanbas/Courses/Modules/ModulesControls.tsx
+++ src/Kanbas/Courses/Modules/ModulesControls.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaPlus } from "react-icons/fa6";
 import GreenCheckmark from "./GreenCheckmark";
 import ModuleEditor from "./ModuleEditor";
+import { useSelector } from "react-redux";
 
 interface ModulesControlsProps {
   moduleName: string;
@@ -14,89 +15,113 @@ export default function ModulesControls({
   setModuleName,
   addModule,
 }: ModulesControlsProps) {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+
   return (
     <div id="wd-modules-controls" className="text-nowrap">
-      <button
-        id="wd-add-module-btn"
-        className="btn btn-lg btn-danger me-1 float-end"
-        data-bs-toggle="modal"
-        data-bs-target="#wd-add-module-dialog"
-      >
-        <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-        Module
-      </button>
-      <div className="dropdown d-inline me-1 float-end">
-        <button
-          id="wd-publish-all-btn"
-          className="btn btn-lg btn-secondary dropdown-toggle"
-          type="button"
-          data-bs-toggle="dropdown"
-        >
-          <GreenCheckmark />
-          Publish All
-        </button>
-        <ul className="dropdown-menu">
-          <li>
-            <a
-              id="wd-publish-all-modules-and-items-btn"
-              className="dropdown-item"
-              href="#"
+      {/* Render all controls for FACULTY, and limited controls for others */}
+      {currentUser.role === "FACULTY" ? (
+        <>
+          <button
+            id="wd-add-module-btn"
+            className="btn btn-lg btn-danger me-1 float-end"
+            data-bs-toggle="modal"
+            data-bs-target="#wd-add-module-dialog"
+          >
+            <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+            Module
+          </button>
+          <div className="dropdown d-inline me-1 float-end">
+            <button
+              id="wd-publish-all-btn"
+              className="btn btn-lg btn-secondary dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
             >
               <GreenCheckmark />
-              Publish all modules and items
-            </a>
-          </li>
-          <li>
-            <a
-              id="wd-publish-modules-only-button"
-              className="dropdown-item"
-              href="#"
-            >
-              <GreenCheckmark />
-              Publish modules only
-            </a>
-          </li>
-          <li>
-            <a
-              id="wd-unpublish-all-modules-and-items"
-              className="dropdown-item"
-              href="#"
-            >
-              Unpublish all modules and items
-            </a>
-          </li>
-          <li>
-            <a
-              id="wd-unpublish-modules-only"
-              className="dropdown-item"
-              href="#"
-            >
-              Unpublish modules only
-            </a>
-          </li>
-        </ul>
-      </div>
-      <button
-        id="wd-view-progress"
-        className="btn btn-lg btn-secondary me-1 float-end"
-        type="button"
-      >
-        Collapse All
-      </button>
-      <button
-        id="wd-collapse-all"
-        className="btn btn-lg btn-secondary me-1 float-end"
-        type="button"
-      >
-        View Progress
-      </button>
-
-      <ModuleEditor
-        dialogTitle="Add Module"
-        moduleName={moduleName}
-        setModuleName={setModuleName}
-        addModule={addModule}
-      />
+              Publish All
+            </button>
+            <ul className="dropdown-menu">
+              <li>
+                <a
+                  id="wd-publish-all-modules-and-items-btn"
+                  className="dropdown-item"
+                  href="#"
+                >
+                  <GreenCheckmark />
+                  Publish all modules and items
+                </a>
+              </li>
+              <li>
+                <a
+                  id="wd-publish-modules-only-button"
+                  className="dropdown-item"
+                  href="#"
+                >
+                  <GreenCheckmark />
+                  Publish modules only
+                </a>
+              </li>
+              <li>
+                <a
+                  id="wd-unpublish-all-modules-and-items"
+                  className="dropdown-item"
+                  href="#"
+                >
+                  Unpublish all modules and items
+                </a>
+              </li>
+              <li>
+                <a
+                  id="wd-unpublish-modules-only"
+                  className="dropdown-item"
+                  href="#"
+                >
+                  Unpublish modules only
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button
+            id="wd-view-progress"
+            className="btn btn-lg btn-secondary me-1 float-end"
+            type="button"
+          >
+            Collapse All
+          </button>
+          <button
+            id="wd-collapse-all"
+            className="btn btn-lg btn-secondary me-1 float-end"
+            type="button"
+          >
+            View Progress
+          </button>
+          <ModuleEditor
+            dialogTitle="Add Module"
+            moduleName={moduleName}
+            setModuleName={setModuleName}
+            addModule={addModule}
+          />
+        </>
+      ) : (
+        // Non-faculty view: only "Collapse All" and "View Progress" buttons
+        <>
+          <button
+            id="wd-view-progress"
+            className="btn btn-lg btn-secondary me-1 float-end"
+            type="button"
+          >
+            Collapse All
+          </button>
+          <button
+            id="wd-collapse-all"
+            className="btn btn-lg btn-secondary me-1 float-end"
+            type="button"
+          >
+            View Progress
+          </button>
+        </>
+      )}
     </div>
   );
 }
