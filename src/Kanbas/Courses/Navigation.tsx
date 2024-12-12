1a8301b949ecf7fc8e1f87521a26716b510818c5
Jeff Zhao <mason8971@gmail.com>
2024-12-12 18:21:08 -0500

    kind of ok

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,23 +1,21 @@
 import { Link, useParams, useLocation } from "react-router-dom";
-import { courses } from "../Database";
-import "../styles.css";
 
 export default function CoursesNavigation() {
   const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
   const { cid } = useParams();
-  const course = courses.find((course) => course._id === cid);
   const { pathname } = useLocation();
 
   return (
     <div>
-      {/* Navigation Links without Breadcrumb */}
       <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
         {links.map((link, index) => (
           <Link
             key={index}
             id={`wd-course-${link.toLowerCase()}-link`}
-            className={`list-group-item border border-0 ${pathname.includes(link) ? "active" : "text-danger"}`}
-            to={`/Kanbas/Courses/${course?._id}/${link}`}
+            className={`list-group-item border border-0 ${
+              pathname.includes(link) ? "active" : "text-danger"
+            }`}
+            to={`/Kanbas/Courses/${cid}/${link}`}
           >
             {link}
           </Link>
